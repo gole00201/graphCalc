0a0e8f4382caa65c4618335a8fe2dfb5b8657c40
volkov <volkov@crtc.spb.ru>
2023-06-07 11:20:29 +0300

    Add docs

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import math
 
 
 def cnt_dig(number, base):
+    '''Считаем число разрядов в системе, чтоб ыописать число'''
     return math.ceil(math.log(number + 1, base))
 
 
 def change_sys(num, t):
+    '''Возвращаем генератор со знаками числа в системе'''
     assert num > -1 and t >= 2
     cnt = 0
     dig = cnt_dig(num, t)
@@ -17,6 +19,8 @@ def change_sys(num, t):
 
 
 def change_mult_sys(list_of_nums, t):
+    '''Делаем change_sys для каждого элемента в списке
+    вовзращаем генератор списков'''
     for num in list_of_nums:
         tmp = list(change_sys(num, t))
         tmp.reverse()
@@ -24,6 +28,7 @@ def change_mult_sys(list_of_nums, t):
 
 
 def create_data(maunt):
+    '''просто генерим список чисел'''
     return [i for i in range(maunt+1)]
 
 
